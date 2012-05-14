5d8584e95717d0b639726a4b8c93915053bd2a2e
Andy Beers <acb75@pitt.edu>
2012-05-13 22:22:23 -0400

    Perl

---
+++ Perl/bool.pl
@@ -0,0 +1,5 @@
+#boolean examples
+use warnings; 
+
+print " 5 >= 4? ", 5>=4, "\n";
+print " 4 =3 ? " , 4==3, "\n";
---
+++ Perl/numbers.pl
@@ -0,0 +1,7 @@
+# Perl number systems Written By Andrew Beers
+
+use warnings;
+print 255, "\n";
+print 0377, "\n";
+print 0b11111111, "\n";
+print 0xff, "\n";
